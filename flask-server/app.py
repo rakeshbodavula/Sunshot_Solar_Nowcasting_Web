250ca2224165905bc793b8d73f1c0402da0db282
RAKESH BODAVULA <rakeshbodavula2002@gmail.com>
2023-05-20 21:22:24 +0530

    advancing to deploy

--- flask-server/app.py
+++ flask-server/app.py
@@ -3,9 +3,12 @@ from PIL import Image
 import numpy as np
 import joblib
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
+env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 # Load your trained model
 model = joblib.load('./trained_models/HGBR.sav')
