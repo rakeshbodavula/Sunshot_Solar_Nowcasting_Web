868680a4528f4dacf223ad71c73e3536a97b706b
RAKESH BODAVULA <rakeshbodavula2002@gmail.com>
2023-05-20 20:29:36 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'; // Import CSS file for styling
+
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+    setPreviewImage(URL.createObjectURL(file)); // Store image URL for preview
+  };
+
+  const handlePrediction = async () => {
+    const formData = new FormData();
+    formData.append('image', selectedImage);
+
+    try {
+      const response = await axios.post('http://localhost:5000/predict', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(response.data); // Check response data in console
+      setPrediction(response.data.prediction[0]); // Update prediction state
+    } catch (error) {
+      console.error('Error occurred during prediction:', error);
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1 className="app-title">Sunshot Solar Nowcasting</h1>
+      <p><span>How it works: </span>
+        Upload a sky image and you will get the PV value generated in Kilo Watts for that image.</p>
+      <input type="file" onChange={handleImageUpload} className="file-input" />
+      <button onClick={handlePrediction} className="predict-button">Predict</button>
+      <h2>Uploaded Image: </h2>
+      {previewImage && <img src={previewImage} alt="Uploaded" className="preview-image" />} {/* Display uploaded image */}
+      {prediction && <p className="prediction">PV value Predicted: {prediction} KW</p>} {/* Render prediction value */}
+    </div>
+  );
+};
+
+export default App;
---
+++ flask-server/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+import numpy as np
+import joblib
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Load your trained model
+model = joblib.load('./trained_models/HGBR.sav')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image found'})
+    image = request.files['image']
+    # Load the image
+    image = Image.open(image)
+
+    # Resize the image to 64x64
+    resized_image = image.resize((64, 64))
+
+    # Convert the resized image to RGB
+    rgb_image = resized_image.convert('RGB')
+
+    # Convert the RGB image to a 1D array
+    array_1d = np.array(rgb_image).reshape(12288)
+    prediction = model.predict([array_1d])
+    # Perform further processing on the prediction if needed
+    return jsonify({'prediction': list(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
